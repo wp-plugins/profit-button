e09675f74b0ebd88b7ea4b3ffb2d58ff76d52faa
hintsolutions <hintsolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-15 19:14:47 +0000

    Updated probtn library
    git-svn-id: https://plugins.svn.wordpress.org/profit-button@805035 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/start_probtn.php
+++ trunk/start_probtn.php
@@ -1,6 +1,9 @@
+<?php
+header("content-type: application/javascript");
+?>
 jQuery(document).ready(function() {
 	jQuery(document).StartButton({
-		"mainStyleCss": "<?php echo $_GET["mainStyleCss"]; ?>",
+		"mainStyleCss": "https://pizzabtn.herokuapp.com/stylesheets/probtn.css",
 		"jqueryPepPath": "<?php echo $_GET["jqueryPepPath"]; ?>",
 	});
 });
