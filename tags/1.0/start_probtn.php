c306d8ba3b9d95a339f6c5c51d1a437cf170aab3
hintsolutions <hintsolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-17 21:06:10 +0000

    Removed incorrect files from tag
    git-svn-id: https://plugins.svn.wordpress.org/profit-button/tags/1.1@805926 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/1.0/start_probtn.php
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-	jQuery(document).StartButton({
-		"mainStyleCss": "<?php echo $_GET["mainStyleCss"]; ?>",
-		"jqueryPepPath": "<?php echo $_GET["jqueryPepPath"]; ?>",
-	});
-});
